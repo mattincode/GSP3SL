84bf3bdacab7595c4f852e2f497d78816032ca04
titte <titte@telia.com>
2016-06-14 15:02:03 +0200

    initial commit, working one way communcation example

--- GSP3SL/MainPage.xaml.cs
+++ GSP3SL/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Browser;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,6 +18,32 @@ namespace GSP3SL
         public MainPage()
         {
             InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            HtmlPage.RegisterScriptableObject("mySLapp", new HTMLWorker(this));
+        }
+
+    }
+
+
+    public class HTMLWorker
+    {
+        MainPage _ctl = null;
+        public HTMLWorker(MainPage ctl)
+        {
+            _ctl = ctl;
+        }
+
+        [ScriptableMember()]
+        public void ChangeTabbis()
+        {
+            if (_ctl.Tabbis.SelectedIndex == 0)
+                _ctl.Tabbis.SelectedIndex = 1;
+            else
+                _ctl.Tabbis.SelectedIndex = 0;
         }
     }
 }
